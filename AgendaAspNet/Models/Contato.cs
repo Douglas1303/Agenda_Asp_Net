a75559522454a76269981a390d958147f8857769
Douglas Santos <Douglassantos6@outlook.com>
2020-05-26 23:34:08 -0300

    commit Final

---
+++ AgendaAspNet/Models/Contato.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgendaAspNet.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "o campo {0} é obrigatório")]
+        [StringLength(200, ErrorMessage = "o campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "o campo {0} é obrigatório")]
+        [RegularExpression("^(?!0+$)(\\+\\d{1,3}[- ]?)?(?!0+$)\\d{10,15}$", ErrorMessage = "Digite um numero de telefone valido.")]
+        public string Telefone { get; set; }
+    }
+}
